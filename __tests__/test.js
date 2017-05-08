c426023d9e2a24dffbe3d5ad34582edab893024b
Alan Nosenkis <nalanpa@yandex.ru>
2017-05-08 22:06:55 +0300

    write data to file & test

--- __tests__/test.js
+++ __tests__/test.js
@@ -4,10 +4,10 @@ import nock from 'nock';
 import path from 'path';
 // import axios from '../src/lib/axios';
 import pageLoad from '../src';
+import generateName from '../src/lib/file_name_generator';
 
 
 const host = 'http://www.example.com';
-const pageName = 'page.html';
 const testData = 'Some output';
 const absentPageError = 'Page is absent';
 
@@ -16,18 +16,37 @@ describe('test page loader', () => {
   beforeEach(() => {
     tempDir = fs.mkdtempSync(`${os.tmpdir()}/`);
     nock(host)
-      .get('/test')
+      .get('/')
       .reply(200, testData);
     nock(host)
       .get('/absent_page')
       .reply(404, absentPageError);
   });
 
-  it('message test', () => {
-    const expected = `Url: ${host}, output: ${tempDir}`;
-    expect.assertions(1);
-    return pageLoad(host, tempDir).then((message) => {
-      expect(message).toBe(expected);
-    });
+  it('No errors', (done) => {
+    const url = `${host}`;
+    const expectedMessage = 'Ok';
+    const fileName = generateName(url);
+    const filePath = path.resolve(tempDir, fileName);
+
+    return pageLoad(url, tempDir)
+      .then((message) => {
+        expect(message).toBe(expectedMessage);
+      })
+      .then(() => {
+        expect(fs.readFileSync(filePath, 'utf8')).toBe(testData);
+      })
+      .then(done)
+      .catch(done.fail);
+  });
+
+  it('Error 404', async () => {
+    const url = `${host}/absent_page`;
+    const expectedMessage = `ERROR: File isn't found by url ${url}`;
+    try {
+      await pageLoad(url, tempDir);
+    } catch (error) {
+      expect(error).toBe(expectedMessage);
+    }
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-// import axios from './lib/axios';
+import fs from 'mz/fs';
+import path from 'path';
+import axios from './lib/axios';
+import generateName from './lib/file_name_generator';
 
-export default (url, output = '.') => {
-  const message = `Url: ${url}, output: ${output}`;
-  return new Promise((resolve) => {
-    resolve(message);
-  });
+
+export default (url, outputPath = '.') => {
+  const fileName = generateName(url);
+  const filePath = path.resolve(outputPath, fileName);
+
+  return axios
+    .get(url)
+    .then(result => result.data)
+    .then(data => fs.writeFile(filePath, data))
+    .then(() => 'Ok')
+    .catch((error) => {
+      if (error.response) {
+        if (error.response.status === 404) {
+          return Promise.reject(`ERROR: File isn't found by url ${error.config.url}`);
+        }
+      }
+      return Promise.reject(`ERROR: ${error.code}`);
+    });
 };
